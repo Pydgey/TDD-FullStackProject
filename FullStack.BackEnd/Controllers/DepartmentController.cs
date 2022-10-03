6c762eb868fbbb540347e7886dd199fb984289e4
Felipe Ribeiro Reis <felipe.reis@domore.com.br>
2022-10-03 20:39:25 -0300

    initial backend and tests

---
+++ FullStack.BackEnd/Controllers/DepartmentController.cs
@@ -0,0 +1,157 @@
+﻿using fullstackBackEnd.Models;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace fullstackBackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : Controller
+    {
+        private readonly MySqlConnectionStringBuilder connection;
+        public DepartmentController ()
+        {
+            connection = new MySqlConnectionStringBuilder();
+            connection.Server = "127.0.0.1";
+            connection.Port = 3308;
+            connection.UserID = "root";
+            connection.Database = "mytestdb";
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            try
+            {
+                string query = @"select DepartmentId,DepartmentName from Department";
+
+                DataTable table = new DataTable();
+                MySqlDataReader myReader;
+
+                using (MySqlConnection mycon = new MySqlConnection(connection.ToString()))
+                {
+                    mycon.Open();
+                    using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                    {
+                        myReader = myCommand.ExecuteReader();
+                        table.Load(myReader);
+
+                        myReader.Close();
+                        mycon.Close();
+                    }
+                }
+
+                return new JsonResult(table);
+            }
+            catch
+            {
+                throw new Exception("Get Error");
+            }
+
+        }
+
+        [HttpPost]
+        public JsonResult Post(Department department)
+        {
+            try
+            {
+                string query = @"insert into Department (DepartmentName) Values (@DepartmentName)";
+
+                DataTable table = new DataTable();
+                MySqlDataReader myReader;
+
+                using (MySqlConnection mycon = new MySqlConnection(connection.ToString()))
+                {
+                    mycon.Open();
+                    using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                    {
+                        myCommand.Parameters.AddWithValue("@DepartmentName", department.DepartmentName);
+
+                        myReader = myCommand.ExecuteReader();
+                        table.Load(myReader);
+
+                        myReader.Close();
+                        mycon.Close();
+                    }
+                }
+
+                return new JsonResult("Added Successfully");
+            }
+            catch
+            {
+                throw new Exception("Get Error");
+            }
+
+        }
+
+        [HttpPut]
+        public JsonResult Put(Department department)
+        {
+            try
+            {
+                string query = @"update Department set DepartmentName = @DepartmentName where DepartmentId = @DepartmentId";
+
+                DataTable table = new DataTable();
+                MySqlDataReader myReader;
+
+                using (MySqlConnection mycon = new MySqlConnection(connection.ToString()))
+                {
+                    mycon.Open();
+                    using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                    {
+                        myCommand.Parameters.AddWithValue("@DepartmentName", department.DepartmentName);
+                        myCommand.Parameters.AddWithValue("@DepartmentId", department.DepartmentId);
+
+                        myReader = myCommand.ExecuteReader();
+                        table.Load(myReader);
+
+                        myReader.Close();
+                        mycon.Close();
+                    }
+                }
+
+                return new JsonResult("Updated Successfully");
+            }
+            catch
+            {
+                throw new Exception("Get Error");
+            }
+
+        }
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            try
+            {
+                string query = @"delete from Department where DepartmentId = @DepartmentId";
+
+                DataTable table = new DataTable();
+                MySqlDataReader myReader;
+
+                using (MySqlConnection mycon = new MySqlConnection(connection.ToString()))
+                {
+                    mycon.Open();
+                    using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                    {
+                        myCommand.Parameters.AddWithValue("@DepartmentId", id);
+
+                        myReader = myCommand.ExecuteReader();
+                        table.Load(myReader);
+
+                        myReader.Close();
+                        mycon.Close();
+                    }
+                }
+
+                return new JsonResult("Deleted Successfully");
+            }
+            catch
+            {
+                throw new Exception("Get Error");
+            }
+
+        }
+    }
+}
---
+++ FullStack.Tests/BackEnd/DepartmentTest.cs
@@ -0,0 +1,62 @@
+﻿using fullstackBackEnd.Controllers;
+using fullstackBackEnd.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1.BackEnd
+{
+    public class DepartmentTest : IDisposable
+    {
+        private readonly DepartmentController _controller;
+        public ITestOutputHelper SaidaConsole;
+        public DepartmentTest ( ITestOutputHelper _saidaConsole)
+        {
+            _controller = new DepartmentController();
+            SaidaConsole = _saidaConsole;
+            SaidaConsole.WriteLine("Constructor invocado", _controller);
+        }
+
+        [Fact]
+        public void TestGet()
+        {
+            var results = _controller.Get();
+            SaidaConsole.WriteLine(results.StatusCode.GetValueOrDefault().ToString());
+            Assert.IsType<JsonResult>(results);
+        }
+
+        [Fact]
+        public void TestPost()
+        {
+            Department department = new Department();
+            department.DepartmentName = "TesteQA";
+            var results = _controller.Post(department);
+            Assert.IsType<JsonResult>(results);
+        }
+
+        [Fact]
+        public void TestPut()
+        {
+            Department department = new Department();
+            department.DepartmentName = "TesteXunit";
+            department.DepartmentId = 5;
+            var results = _controller.Put(department);
+            Assert.IsType<JsonResult>(results);
+        }
+
+        [Fact]
+        public void TestDelete()
+        {
+            int departmentId = 5;
+            var results = _controller.Delete(departmentId);
+            Assert.IsType<JsonResult>(results);
+        }
+        public void Dispose()
+        {
+            SaidaConsole.WriteLine("Dispose");
+        }
+    }
+}
